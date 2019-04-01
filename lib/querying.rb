054cf15aac8a3c7c0877658ecd758e8ea7bbe954
Richard Yance <yance0022@gmail.com>
2019-04-01 22:46:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM characters
+  "SELECT series.title FROM characters 
   INNER JOIN series ON characters.series_id = series.id
   GROUP BY series.title, species HAVING characters.species = 'human'
   ORDER BY COUNT(species) DESC
@@ -31,5 +31,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(name) FROM character_books
+  INNER JOIN characters ON characters.id = character_books.character_id
+  GROUP BY characters.name
+  ORDER BY COUNT(name) DESC;"
 end
